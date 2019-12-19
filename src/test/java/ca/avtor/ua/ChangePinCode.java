f700aa7764a72c4cc7956a1c1477e8de71198abc
MechanicalMac <Mechanical_Mac@icloud.com>
2019-12-19 11:40:50 +0200

    Second commit. Add new class ChangePinCode.

---
+++ src/test/java/ca/avtor/ua/ChangePinCode.java
@@ -0,0 +1,15 @@
+package ca.avtor.ua;
+
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@RunWith(JUnitParamsRunner.class)
+public class ChangePinCode {
+    @Test
+    public void changePinCode() {
+        ChromeDriver chromeDriver = new Driver().getDriverInstance();
+        
+    }
+}
--- src/test/java/ca/avtor/ua/Driver.java
+++ src/test/java/ca/avtor/ua/Driver.java
@@ -2,12 +2,15 @@ package ca.avtor.ua;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
     private ChromeDriver driver;
     Driver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     public ChromeDriver getDriverInstance(){
