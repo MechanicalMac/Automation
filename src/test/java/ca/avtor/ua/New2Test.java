5297e22163ec9731b11e0568804138348af56583
MechanicalMac <Mechanical_Mac@icloud.com>
2020-01-08 20:03:36 +0200

    Some shit. Second edition.

---
+++ src/test/java/ca/avtor/ua/New2Test.java
@@ -0,0 +1,11 @@
+package ca.avtor.ua;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class New2Test {
+    @Test
+    public void test2() {
+        Assert.assertTrue(false);
+    }
+}
